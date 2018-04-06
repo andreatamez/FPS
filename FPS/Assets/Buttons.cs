42aee1277fcaf71f005b55b631272580a6e64fb3
andreatamez <fabiolatamez@outlook.com>
2018-04-05 21:13:00 -0500

    Adds Gun Script and Obstacles

---
+++ FPS/Assets/Buttons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("escape"))
+			Application.Quit();
+	}
+
+	public void BtnInstrucciones(){
+		Application.LoadLevel("Instrucciones");
+	}
+
+	public void BtnInicio(){
+		Application.LoadLevel("MainScene");
+	}
+
+	public void BtnSalir(){
+		Application.Quit();
+	}
+}
