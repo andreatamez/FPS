4a429db65c65bc30f202366a3ec246bc6ccedb6a
EliasMera <a01280762@itesm.mx>
2018-04-05 22:38:31 -0500

    Adds sounds and destroys objects

--- FPS/Assets/Gun.cs
+++ FPS/Assets/Gun.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-
+[RequireComponent(typeof(AudioSource))]
 public class Gun : MonoBehaviour {
 
 	public Camera fpsCam;
@@ -8,6 +8,8 @@ public class Gun : MonoBehaviour {
 	public float damage = 10f;
 	//range a donde puede llegar 
 	public float range = 100f;
+	private int capsulesDestroyed = 0;
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -15,6 +17,10 @@ public class Gun : MonoBehaviour {
 		//LeftMouse Button ya decidido por unity
 		if (Input.GetButtonDown ("Fire1")) {
 			Shoot ();
+		}
+
+		if (Input.GetKey ("escape")) {
+			Application.Quit ();
 		}
 	}
 
@@ -26,6 +32,14 @@ public class Gun : MonoBehaviour {
 
 			//te da el nombre de lo que golpeaste
 			Debug.Log (hit.transform.name);
+			if (hit.transform.name.ToString () == "Capsule") {
+				capsulesDestroyed++;
+				Destroy (hit.transform.gameObject);
+				if (capsulesDestroyed == 6) {
+					Debug.Log ("Ganaste");
+					Application.LoadLevel ("WIN");
+				}
+			}
 		}
 	}
 }
--- FPS/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ FPS/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -27,6 +27,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
         [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
         [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
+		[SerializeField] private AudioClip m_fire;
 
         private Camera m_Camera;
         private bool m_Jump;
@@ -79,6 +80,11 @@ namespace UnityStandardAssets.Characters.FirstPerson
             {
                 m_MoveDir.y = 0f;
             }
+
+			if (Input.GetButtonDown ("Fire1")) {
+				m_AudioSource.clip = m_fire;
+				m_AudioSource.Play ();
+			}
 
             m_PreviouslyGrounded = m_CharacterController.isGrounded;
         }
