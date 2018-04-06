db41de923adfeae8d22b5504ba1d4da7ab30506c
andreatamez <fabiolatamez@outlook.com>
2018-04-05 21:19:57 -0500

    Adds comments to script C#

--- FPS/Assets/Gun.cs
+++ FPS/Assets/Gun.cs
@@ -1,19 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Gun : MonoBehaviour {
 
 	public Camera fpsCam;
 
-
+	//damage que da al objeto
 	public float damage = 10f;
+	//range a donde puede llegar 
 	public float range = 100f;
 	
 	// Update is called once per frame
 	void Update () {
 
-		//Esto funciona para el Shoot
+		//LeftMouse Button ya decidido por unity
 		if (Input.GetButtonDown ("Fire1")) {
 			Shoot ();
 		}
